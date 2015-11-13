03e80584aab1038ac9c17f3ea167847975c694ef
schmitz1193 <schmitz1193@gmail.com>
2015-11-13 11:45:42 -0600

    added CSS styel

--- javascripts/promises.js
+++ javascripts/promises.js
@@ -19,7 +19,12 @@ requirejs(
 //Here's some pseudo-code for how it should look once you start using promises
   var booktype = "";
   var bookinfo = "";
-
+    //a function is called to make an API call, while it is running the promise that it
+    //will resolve is returned.  "then" when it is resolved the data is returned
+    // and assigned to a variable so it can be accessed later. AND a second API call is made
+    // a promise is returned and when that call is resolved the data is manipulated and the format
+    // to put to HBS is called.
+    
     types.load()
       .then(function(types) {
         console.log("API-types call successful and responded with", types);
@@ -42,7 +47,7 @@ requirejs(
                book.type = _.find(types, { id:book.booktype }).label;
               return book;
             });
-            
+
            console.log("bookformat ", bookformat);
 
               // then bind the template to the data ----Call a format.js template module"
